87d6bf7f49109e8f45d3c445eea73e575a4e2f0e
jhlee <jhlee@spicusinc.com>
2025-03-07 00:26:11 +0900

    feat: 격자 위경도, 단기예보 추가

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,23 @@
+import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
+
+dotenv.config();
 
 const {
   DB_HOST,
   DB_USER,
   DB_PASSWORD,
   DB_NAME,
+  DB_PORT,
 } = process.env;
 
 // 데이터베이스 연결
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
+  dialectModule: mysql2,
+  port: DB_PORT,
   logging: false,
 });
 
